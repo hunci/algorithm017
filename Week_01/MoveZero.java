cfd8f44a90efce78670e300c196f3d49436de002
吕云辉 <lvyunhui@qiyi.com>
2020-09-25 13:07:37 +0800

    modify for mergeList

--- Week_01/MoveZero.java
+++ Week_01/MoveZero.java
@@ -12,4 +12,17 @@ public class MoveZero {
         }
 
     }
+
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length -1;
+        int res = 0;
+        while(i < j){
+            res = height[i] < height[j] ?
+                  Math.max(res, (j - i) * height[i++])  :
+                  Math.max(res, (j - i) * height[j--])
+
+        }
+        return res;
+    }
 }
